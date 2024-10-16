d2b4ca7551962debdb0dbc81afff2e764bf1d788
eiianacarolina <161865425+eiianacarolina@users.noreply.github.com>
2024-10-16 15:50:01 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,12 @@ Route::get('tarefa/find/id', [TarefaController::class, 'findByIdRequest']);
 
 Route::get('tarefa/{id}', [TarefaController::class, 'findById']);
 
+Route::get('tarefa', [TarefaController::class, 'getAll']);
 
+Route::get('tarefa/find/titulo', [TarefaController::class, 'findByName']);
 
+Route::delete('tarefa', [TarefaController::class, 'delete']);
 
+Route::put('tarefa',[TarefaController::class, 'update']);
 
+Route::get('tarefa/find/dia_mes', [TarefaController::class, 'where']);
