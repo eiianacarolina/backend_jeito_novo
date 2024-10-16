d5c1343272ad1cbaf2e37c1b8c4e37becc0a86a3
eiianacarolina <161865425+eiianacarolina@users.noreply.github.com>
2024-10-16 15:49:18 -0300

    Update TarefaController.php

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -55,4 +55,96 @@ class TarefaController extends Controller
             'data'=>$tarefa
         ]);
     }
+     public function getAll(){
+        $tarefa = Tarefa::all();
+
+        return response()->json([
+            'status' => true,
+            'data'=>$tarefa
+        ]);
+    }
+
+    public function findByName(Request $request){
+        $tarefa = Tarefa::where('titulo', 'like', '%' . $request->titulo . '%')->get();
+
+        if($tarefa->isEmpty()){
+            return response()->json([
+                'status' => false,
+                'message'=> 'Usuário não encontrado',
+            ]);
+        }
+
+        return response()->json([
+            'status' => true,
+            'message'=> 'Usuário encontrado',
+            'data'=>$tarefa
+        ]);
+    }
+
+    public function delete(Request $request){
+        $tarefa = Tarefa::find($request->id);
+
+        if($tarefa == null){
+            return response()->json([
+                'status' => false,
+                'message'=> 'Usuário não encontrado',
+            ]);
+        }
+
+        $tarefa->delete();
+
+        return response()->json([
+            'status' => true,
+            'message'=> 'Usuário excluído com sucesso!!'
+        ]);
+    }
+
+    public function update(Request $request){
+        $tarefa = Tarefa::find($request->id);
+
+       if($tarefa == null){
+         return response()->json()([
+            'status'=> false,
+            'message'=> "Usuário não encontrado"
+         ]);
+       }
+
+       if(isset($request->titulo)){
+        $tarefa->titulo = $request->titulo;
+       }
+
+       if(isset($request->descricao)){
+        $tarefa->descricao=$request->descricao;
+       }
+
+       if(isset($request->status)){
+        $tarefa->status=$request->status;
+       }
+
+       $tarefa->update();
+
+       return response()->json([
+        'status'=>true,
+        'message'=> "Usuário atualizado com sucesso!"
+       ]);
+    }
+
+    public function where(Request $request){
+        $tarefa = Tarefa::whereDay( 'created_at', '=', $request->dia)->whereMonth( 'created_at', '=', $request->mes )->get();
+
+        if($tarefa->isEmpty()){
+            return response()->json([
+                'status'=> false,
+                'message'=> 'Usuário não encontrado'
+            ]);
+        }
+
+        return response()->json([
+            'status'=> true,
+            'message'=> 'Usuário encontrado',
+            'data'=> $tarefa
+        ]);
+
+       
+    }
 }
